cc98d3d9e8ccf3dc6a9f1fd8166f5bea43c452ed
Arlene404 <giraarle080@gmail.com>
2023-03-14 09:41:00 +0200

    Display products in template labeled hello.html

--- store/views.py
+++ store/views.py
@@ -7,8 +7,8 @@ from store.models import Product
 def say_hello(request):
     query_set = Product.objects.all()
 
-    for product in query_set:
-        print(product)
+    # for product in query_set:
+    #     print(product)
 
 
     return render (request, 'hello.html', {'products': query_set})
